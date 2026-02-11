f8c40d698347a583db65245f53e4f9b9a1873939
Gunay Hacizade <ghacizade3@std.beu.edu.az>
2026-02-11 22:51:22 +0400

    Create Problem24-Lexicographic_Permutations.py

---
+++ Problem24-Lexicographic_Permutations.py
@@ -0,0 +1,11 @@
+from itertools import permutations
+a = [0,1,2,3,4,5,6,7,8,9]
+listof=[]
+num=0
+for j in permutations(a):
+#make permutations available
+    listof.append(j)
+for i in range(0,10):
+    num+=listof[(10**6)-1][i]*10**(9-i)
+#print(list[0][0]*100+list[0][1]*10+list[0][2])
+print(num)
