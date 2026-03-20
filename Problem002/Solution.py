bc155bf7cbd59b918e027eee2049424b1a66996b
Gunay Hacizade <260498499+Gny-Hczd-me@users.noreply.github.com>
2026-03-20 21:17:35 +0400

    Update Solution.py

--- Problem002/Solution.py
+++ Problem002/Solution.py
@@ -1,10 +1,10 @@
-num1,num2=1,2
-sum=num1+num2
-sum_of_even=2
-while sum<4*10**6:
+num1, num2=1,2
+next_fib=num1+num2
+sum_of_even=2 #the first even Fibonacci number
+while next_fib<4_000_000:
 	num1=num2
-	num2=sum
-	sum=num1+num2
+	num2=next_fib
+	next_fib=num1+num2
 	if num2%2==0:
 		#sum of even-valued Fibonacci numbers
 		sum_of_even+=num2
