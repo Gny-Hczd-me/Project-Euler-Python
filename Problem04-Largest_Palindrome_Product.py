959b4a8790b369a874e253de4cbeb1271e5c4acf
Gunay Hacizade <260498499+Gny-Hczd-me@users.noreply.github.com>
2026-03-20 21:22:40 +0400

    Delete Problem04-Largest_Palindrome_Product.py

--- Problem04-Largest_Palindrome_Product.py
+++
@@ -1,9 +0,0 @@
-maxium=100*100
-for a in range(100,1000):
-	for b in range(100,1000):
-		if str(a*b)==str(a*b)[::-1]:
-			if maxium<a*b:
-				maxium=a*b
-#largest palindrome made from the product of two -digit numbers:
-print(maxium)
-		
