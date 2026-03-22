6e4ca12625ba4d7ace3d4bfc7c3dd35a59d4bf81
Gunay Hacizade <260498499+Gny-Hczd-me@users.noreply.github.com>
2026-03-22 19:30:49 +0400

    Update Solution.py

--- Problem003/Solution.py
+++ Problem003/Solution.py
@@ -3,10 +3,10 @@ a=600851475143
 max=0
 while num<a:
 	if all(num%i!=0 for i in range(2,int(pow(num,0.5))+1)):
-		if a%num==0:
+		while a%num==0:
+			a=a/num
 			max=num
-			a=a/max
 	num+=1
-print(max)
+print(num)
 #largest prime factor of the number 600851475143
 	
